9d173ace427e7f3a18763bc2a6421ca5eef52fc1
Genesis2010 <dmsgktn1685@naver.com>
2020-03-14 17:24:15 +0900

    Create BankAccount.java

---
+++ Object Oriented Programming/BankAccount.java
@@ -0,0 +1,26 @@
+public class BankAccount {
+    int balance;
+    Person owner; // Person 클래스로 가진 자료형 변수 생성
+
+    // 파라미터 : 입금할 액수 (정수)
+    // 리턴 : 성공여부 (불린)
+
+    boolean deposit(int amount){
+
+    }
+
+    // 파라미터 : 출금할 액수 (정수)
+    // 리턴 : 성공여부 (불린)
+
+    boolean withdraw(int amount){
+        
+    }
+
+    // 첫번째 파라미터 -> 받는 사람(Person)
+    // 두번째 파라미터 -> 이체할 금액(정수)
+    // 리턴 -> 성공여부(불린)
+
+    boolean transfer(Person to, int amount){
+        
+    }
+}
